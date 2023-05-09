c188c7092efa263b4e5c191e4d4ac971b06b110e
kkpagaev <kkpagaev@gmail.com>
2023-05-09 03:54:27 +0300

    add fibonacci to node

--- node/index.js
+++ node/index.js
@@ -3,9 +3,26 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
+function fibonacci(n) {
+  const seq = new Array(n);
+
+  if (n >= 1) {
+    seq[0] = 0;
+  }
+  if (n >= 2) {
+    seq[1] = 1;
+  }
+
+  for (let i = 2; i < n; i++) {
+    seq[i] = seq[i - 1] + seq[i - 2];
+  }
+
+  return seq;
+}
+
+app.get("/", function(req, res) {
   res.render("index", {
-    message: "Hello world",
+    arr: fibonacci(80),
   });
 });
 
