f711b0e2f22fb7698a245afdf05a273c27d56347
kkpagaev <kkpagaev@gmail.com>
2023-05-09 04:25:43 +0300

    crystal fib

--- crystal/src/crystal.cr
+++ crystal/src/crystal.cr
@@ -1,8 +1,24 @@
 require "http/server"
+require "ecr"
+
+def fibonacci(n)
+  seq = Array(UInt64).new(n, 0)
+
+  seq[0] = 0 if n >= 1
+
+  seq[1] = 1 if n >= 2
+
+  (2...(n)).each do |i|
+    seq[i] = seq[i - 1] + seq[i - 2]
+  end
+
+  seq
+end
 
 server = HTTP::Server.new do |context|
-  context.response.content_type = "text/plain"
-  context.response.print "Hello world!"
+  context.response.content_type = "text/html"
+  arr = fibonacci(80)
+  context.response.print ECR.render("views/index.ecr")
 end
 
 address = server.bind_tcp "0.0.0.0", 3002
